8b566e76f772f4619ab4ca3838ab1e04c01ff5a4
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:05:13 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan13-Relational/src/pbo1/pkg10118031/latihan13/relational/PBO110118031Latihan13Relational.java
@@ -0,0 +1,26 @@
+package pbo1.pkg10118031.latihan13.relational;
+
+/**
+ *
+ * @author Vendetta
+ * NAMA              : Rizki Aria Putra
+ * KELAS             : PBO1
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk 
+ *                     membandingkan antara dua variabel
+ */
+public class PBO110118031Latihan13Relational {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+
+    }
+
+}
